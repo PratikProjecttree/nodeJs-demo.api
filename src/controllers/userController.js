14c9d421cb6333b0515c14e11c3d1418779d627e
PratikProjecttree <pratik.vaghasiya@projecttree.in>
2025-05-29 10:36:49 +0530

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,12 +7,13 @@ const message = require('../utils/constants/messages');
 // #endregion
 
 // #region Methods 
-const createUserWithRoles = async (req, res) => {
+
+exports.createUserWithRoles = async (req, res) => {
   const data = await userService.createUserWithRoles(req.body);
   response.success(res, data, message.RECORD_CREATED);
 };
 
-const updateUserWithRoles = async (req, res) => {
+exports.updateUserWithRoles = async (req, res) => {
   const userId = req.params.id;  // get userId from URL like /users/:id
   const { name, roleIds } = req.body;
 
@@ -22,11 +23,6 @@ const updateUserWithRoles = async (req, res) => {
 
   const data = await userService.updateUserWithRoles({ userId, name, roleIds });
   response.success(res, data, message.RECORD_UPDATED);
-};
-
-module.exports = {
-  createUserWithRoles,
-  updateUserWithRoles,
 };
 
 // #endregion
